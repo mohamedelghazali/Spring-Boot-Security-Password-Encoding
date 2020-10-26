0c94cc8b73920248c566f60f99276c178bd3e8c1
Mohamed El Ghazali <48661966+mohamedelghazali@users.noreply.github.com>
2020-10-26 04:42:46 +0100

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,25 @@
+package com.******.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.devglan.service.UserService;
+
+@Controller
+public class DashboardController {
+	
+	@Autowired
+	private UserService userService;
+
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    public ModelAndView dashboard() {
+    	ModelAndView model = new ModelAndView();
+    	model.addObject("users", userService.getUsers());
+    	model.setViewName("dashboard");
+    	return model;
+    }
+
+}
